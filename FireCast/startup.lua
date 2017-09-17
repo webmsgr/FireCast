be53219a1d6e14d06afab32f832d158d7bf7d005
TheMrIron2 <dwelch.appleid@gmail.com>
2017-09-17 13:10:53 +0100

    Update startup.lua

--- FireCast/startup.lua
+++ FireCast/startup.lua
@@ -5,11 +5,18 @@ local function center(y,string)
   print(string)
 end
 
+local function centerSlow(y,string,spd)
+  local w,h = term.getSize()
+  local x = (w/2)-(#string/2)
+  term.setCursorPos(x,y)
+  textutils.slowPrint(string,spd)
+end
+
 term.setBackgroundColour(colours.white)
 term.setTextColour(colours.red)
 term.clear()
 center(6,"Welcome to FireCast.")
 sleep(1.25)
-center(7,"The Ultimate Innovation.")
-sleep(2.25)
+centerSlow(7,"It's thinking.",10)
+sleep(2)
 shell.run("FireCast/Menu")
